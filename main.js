48ca04424b8e691832ae87630d2f50fed6e0003d
womiekom <159969864+womiekom@users.noreply.github.com>
2025-03-07 05:23:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-onload = () =>{
-        document.body.classList.remove("container");
-};
+function startBirthday() {
+    document.getElementById("welcome-screen").style.display = "none";
+    document.getElementById("birthday-content").style.display = "block";
+}
+
+function showSurprise() {
+    const confettiContainer = document.getElementById("confetti-container");
+    confettiContainer.innerHTML = "🎊🎉💖✨🥳";
+    confettiContainer.style.fontSize = "50px";
+
+    // Play birthday song
+    let audio = new Audio('https://www.myinstants.com/media/sounds/happy-birthday-to-you.mp3');
+    audio.play();
+}
